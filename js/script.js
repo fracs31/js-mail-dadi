41d2194627b14f54814b1857cb9080e85b56a5a3
fracs31 <pist.fracs95@gmail.com>
2023-02-03 14:30:13 +0100

    START mail exercise

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let mail = ["emmastone@gmail.com", "tomholland@gmail.com", "bradpitt@gmail.com", "margotrobbie@gmail.com", "sylvesterstallone@gmail.com"]; //array di mail
+let search = prompt("Inserire l'email da cercare: "); //ricerca
+
+console.log(mail);
+
+//Ciclo
+// for (let i = 0; i < mail.length; i++) {
+//     //Se la mail inserita dall'utente è presente nell'array
+//     if (search == mail[i]) {
+        
+//     }
+// }
