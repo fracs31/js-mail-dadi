e4c7bd428720a2083c3b2f98704453f658831b7a
fracs31 <pist.fracs95@gmail.com>
2023-02-03 14:38:25 +0100

    ADD mail research

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 let mail = ["emmastone@gmail.com", "tomholland@gmail.com", "bradpitt@gmail.com", "margotrobbie@gmail.com", "sylvesterstallone@gmail.com"]; //array di mail
 let search = prompt("Inserire l'email da cercare: "); //ricerca
+let found = false; //variabile per controllare se la mail è stata trovata 
 
-console.log(mail);
+console.log(mail); //stampa in console array di mail
+console.log(search); //stampa in console della mail cercata dall'utente
 
 //Ciclo
-// for (let i = 0; i < mail.length; i++) {
-//     //Se la mail inserita dall'utente è presente nell'array
-//     if (search == mail[i]) {
-        
-//     }
-// }
+for (let i = 0; i < mail.length; i++) {
+    //Se la mail inserita dall'utente è presente nell'array
+    if (search == mail[i]) {
+        console.log("MAIL TROVATA " + search + " = " + mail[i]); //stampa in console della mail trovata
+        found = true; //mail trovata
+    }
+}
+
+//Se la mail non è stata trovata
+if (found == false) {
+    console.log("MAIL NON TROVATA"); //stampo in console un messaggio di errore
+}
